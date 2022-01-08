a03ba1644c1a013f5471f1f26aa91e3c9b61c4dd
21ciprian <2021ciprianoffice@gmail.com>
2022-01-08 02:08:52 +0000

    Created addTodo function

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import Input from '../InputComponent'
 import Header from '../Header'
 
 function App() {
+  const [todo, setTodo] = useState([])
+  function addTodo(item) {
+    setTodo([...todo, item])
+  }
   return (
     <div className="App">
       <Header />
-      <Input />
+      <Input onAdd={addTodo} />
     </div>
   );
 }
