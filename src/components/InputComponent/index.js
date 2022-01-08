7d043793110bf5fcd1e27af909e136b266baaff7
21ciprian <2021ciprianoffice@gmail.com>
2022-01-08 02:35:52 +0000

    Created handleChange function for input field

--- src/components/InputComponent/index.js
+++ src/components/InputComponent/index.js
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 function Input() {
   const [text, setText] = useState('')
+
+  function handleChange(event) {
+    const value = event.target.value
+    setText(value)
+  }
   return (
     <div>
-      <input type='text' />
+      <input type='text' onChange={handleChange} value={text} />
       <button>Add</button>
     </div>
   )
