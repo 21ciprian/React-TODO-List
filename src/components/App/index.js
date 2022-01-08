51dc0b3278e1e460c407f207038bdc12dbced569
21ciprian <2021ciprianoffice@gmail.com>
2022-01-08 01:58:36 +0000

    First commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Input from '../InputComponent'
+import Header from '../Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
